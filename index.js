8c1116a9637a5fa2b4fbd5792d31aa14016ba155
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-10-31 17:50:38 +0000

    we'll just see how it goes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
+let quoteLength = quote.length
+let twentySecondLetter = quote[21];
+let output = quote[length - 1, twentySecondLetter];
+
+
+document.getElementById("quote-length").innerHTML = quoteLength;
+document.getElementById("twenty-second-letter").innerHTML = twentySecondLetter;
+document.getElementById("result").innerHTML = output;
