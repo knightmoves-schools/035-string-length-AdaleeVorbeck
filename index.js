4d6df86c17c9e3db2fc02987de721c2d30e98091
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-11-04 20:15:21 +0000

    Yuhhhhhh

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
-let quoteLength = quote.length
+let quoteLength = quote.length;
 let twentySecondLetter = quote[21];
-let output = quote[length - 1];
-let output = quote[twentySecondLetter];
+let output = quote 'The length of quote is $(quoteLength), the twenty second letter is [$(twentySecondLetter)]';
 
-
-document.getElementById("quote-length").innerHTML = quoteLength;
-document.getElementById("twenty-second-letter").innerHTML = twentySecondLetter;
 document.getElementById("result").innerHTML = output;
