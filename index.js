97ac31ad62510f6a8d9c62013c34cade5c6265eb
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-11-04 20:25:29 +0000

    yuhhhh

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let quote = "...ask not what your country can do for you — ask what you can do
 //Add code below
 let quoteLength = quote.length;
 let twentySecondLetter = quote[21];
-let output = quote `The length of quote is ${quoteLength}, the twenty second letter is [${twentySecondLetter}]`;
+let output = `The length of quote is ${quoteLength}, the twenty second letter is [${twentySecondLetter}]`;
 
-document.getElementById("result").innerHTML = output;
+document.getElementById('result').innerHTML = output;
